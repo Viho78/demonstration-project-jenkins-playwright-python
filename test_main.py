ca895c2ee2e4202f873ace48fe87201a2667ddc6
Viho78 <56194262+Viho78@users.noreply.github.com>
2025-12-22 12:48:03 +0100

    beautifing

--- test_main.py
+++ test_main.py
@@ -23,6 +23,7 @@ page_url = r'https://automationintesting.online/'
 @pytest.mark.smoke
 def test_page_opens(page: Page, title_text):
     page.goto(page_url)
+
     # Expect a title to be equal
     assert expect(page).to_have_title(title_text) == None
     
@@ -45,9 +46,11 @@ def test_contact_labels(page: Page, labels):
     page.goto(page_url)
 
     #verify if contact form exists
-    assert expect(page.locator("#contact")) != None
+    assert expect(page.locator("#contact")).to_be_visible() == None
     #verify if contact form contains proper labels
     assert expect(page.locator('#contact')).to_contain_text(labels) == None
+    
+    
 
     
 
