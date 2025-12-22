c71b3ea4aae41a90af7ae2dfcb03b337bf8b013f
Viho78 <56194262+Viho78@users.noreply.github.com>
2025-12-22 10:31:09 +0100

    using fixture for title text

--- test_main.py
+++ test_main.py
@@ -1,24 +1,40 @@
+#------------------------------------------------------------------
 from playwright.sync_api import Page, expect
+
 import pytest
+
 import allure
 
+#------------------------------statics------------------------------------
 #url for training site to practice web ui test automation
 #page is about booking platform for the hotel
 page_url = r'https://automationintesting.online/'
 
+
+
+
+
+#------------------------------tests------------------------------------
+@pytest.fixture
+def title_text():
+    yield "Restful-booker-platform demo"
+
+
+#testing if main page opens correctly
 @allure.suite("SMOKE TESTS")
 @allure.story("Item 105001")
 @allure.label("owner", "JStanczyk")
 @allure.severity(allure.severity_level.BLOCKER)
 @pytest.mark.smoke
-def test_page_opens(page: Page):
+def test_page_opens(page: Page, title_text):
     page.goto(page_url)
-
     # Expect a title to be equal
-    expect(page).to_have_title("Restful-booker-platform demo")
+    assert expect(page).to_have_title(title_text) == None
     # Expects page to have a div with the name of the hotel.
-    expect(page.locator("#root-container")).to_contain_text("Shady Meadows B&B")
+    assert expect(page.locator("#root-container")).to_contain_text("Shady Meadows B&B") == None
 
+
+#testing something
 @allure.suite("SMOKE TESTS")
 @allure.story("Item 105001")
 @allure.label("owner", "JStanczyk")
@@ -26,7 +42,6 @@ def test_page_opens(page: Page):
 @pytest.mark.smoke
 def test_get_started_link(page: Page):
     page.goto(page_url)
-
     pass
 
 if __name__ == "__main__":
