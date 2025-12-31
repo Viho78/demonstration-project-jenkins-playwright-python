fb314fe3ee8ae9c0ec81d0206263787267585d76
Viho78 <56194262+Viho78@users.noreply.github.com>
2025-12-31 10:42:43 +0100

    adding browser selection to conftest.py

--- conftest.py
+++ conftest.py
@@ -5,5 +5,10 @@ import requests
 def title_text():
     yield "Restful-booker-platform demo"
 
-
+#this should be commented, used to run tests locally with arguments. When runned from command line it will forece running only with firefox browser
+@pytest.fixture(scope="session")
+def browser(playwright):
+    browser = playwright.firefox.launch(headless=False, timeout=10000)
+    yield browser
+    browser.close()
 
