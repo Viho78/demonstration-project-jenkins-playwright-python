17de34b44ae92b99dbc0aa48a85fd89398a6389f
Viho78 <56194262+Viho78@users.noreply.github.com>
2026-01-03 19:27:26 +0100

    adding testdata for api for new tests

---
+++ testdata_API.py
@@ -0,0 +1,9 @@
+#CreateBooking
+createbooking_testset_1 = [('John'), ('Doe'), (110), (True), ('2026-01-01'), ('2026-01-03'), ('Breakfast'),]
+createbooking_testset_2 = [('Johnśźć'), ('Doeśźć'), (110), (False), ('2026-01-01'), ('2026-01-03'), (''),]
+createbooking_testset_3 = [(''), (''), (1108), (False), ('2026-01-01'), ('2026-01-03'), (''),]
+
+
+#UpdateBooking
+updatebooking_testset_1 = [('John2'), ('Doe2'), (220), (False), ('2026-01-03'), ('2026-01-04'), ('Breakfast2'),]
+updatebooking_testset_2 = [(''), (''), (''), (''), ('2026-01-03'), ('2026-01-04'), (''),]
