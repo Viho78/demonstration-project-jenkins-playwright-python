c0601e4409053c40a51d5cbd0e9343df56a29bdf
Viho78 <56194262+Viho78@users.noreply.github.com>
2026-01-03 19:29:32 +0100

    Adding fixtre for api token

--- conftest.py
+++ conftest.py
@@ -1,8 +1,22 @@
 import pytest
+import requests
+import secret # file containing @username= and @password= variables
 
 @pytest.fixture
 def title_text():
     yield "Restful-booker-platform demo"
+
+@pytest.fixture
+def get_API_token():
+    url = "https://restful-booker.herokuapp.com/auth"
+    payload = {
+        "username": secret.username,
+        "password": secret.password
+    }
+    response = requests.post(url, json=payload)
+    response.raise_for_status()
+    token = response.json().get("token")
+    yield token
 
 # This should be commented, used only to run tests locally with arguments. 
 # When runned from command line it will forece running only with firefox browser
